a4c36e7cb7eb1d7e6b72883ab9db505ef759a7fb
sub101 <subin10@nate.com>
2023-03-17 13:41:01 +0900

    로그인 연동

--- Bakku/app/src/main/java/com/example/bakku/presentation/LoginActivity.kt
+++ Bakku/app/src/main/java/com/example/bakku/presentation/LoginActivity.kt
@@ -2,39 +2,64 @@ package com.example.bakku.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import com.example.bakku.MainActivity
 import com.example.bakku.R
 import com.example.bakku.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
 class LoginActivity :AppCompatActivity() {
+
     private lateinit var binding : ActivityLoginBinding
+
+    // firebase Auth
     private lateinit var auth : FirebaseAuth
+    // google client
+    private lateinit var googleSignInClient: GoogleSignInClient
+    // private const val TAG = "GoogleActivity"
+    private val RC_SIGN_IN = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        clickBtn()
+        
+        binding.btnLogin.setOnClickListener { clickBtn() }
 
+        //Google 로그인 옵션 구성. requestIdToken 및 Email 요청
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            //'R.string.default_web_client_id' 에는 본인의 클라이언트 아이디를 넣어주면 됌
+            //스트링을 따로 빼서 저렇게 사용했지만 스트링을 통째로 넣어도 됌
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // firebase auth 객체
         auth = Firebase.auth
     }
 
     public override fun onStart() {
         super.onStart()
 
-        val currentUser = auth.currentUser
+        /*val currentUser = auth.currentUser
+        //updateUI(currentUser)
         if(currentUser != null){
             //reload()
+        }*/
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account!==null){ // 이미 로그인 되어있을시 바로 메인 액티비티로 이동
+            toMainActivity(auth.currentUser)
         }
     }
 
@@ -49,4 +74,13 @@ class LoginActivity :AppCompatActivity() {
         }
     }
 
+    // toMainActivity
+    fun toMainActivity(user: FirebaseUser?) {
+        if(user !=null) { // MainActivity 로 이동
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
 }
+
